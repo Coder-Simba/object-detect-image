fa74d9e7ce891373311ac5324f03524000ad6efb
simba <92054547+Coder-Simba@users.noreply.github.com>
2022-08-08 20:55:55 -0400

    Add files via upload

---
+++ house.js
@@ -0,0 +1,53 @@
+img="";
+status = "";
+objects=[];
+
+function preload(){
+    img=loadImage('house.jpg');
+}
+
+function setup(){
+    canvas=createCanvas(640, 420);
+    canvas.position(300, 130);
+    objectDetector=ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML="Status : Detecting Objects";
+}
+
+function modelLoaded(){
+    console.log("Model Loaded!")
+    status=true;
+    objectDetector.detect(img, gotResult);
+}
+
+function draw(){
+    image(img, 0, 0, 640, 420);
+
+    if(status !=""){
+       for(i=0; i< objects.length; i++){
+        document.getElementById("status").innerHTML="Status : Object Detected";
+        fill("#e9d1ab");
+        percent = floor(objects[i].confidence*100);
+        text(objects[i].label+""+percent+"%",objects[i].x, objects[i].y);
+        noFill();
+        stroke("#e9d1ab");
+        rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+       }
+    
+    
+    
+}
+}
+
+
+function gotResult(error, results){
+    if(error){
+        console.log(error);
+
+    }
+    console.log(results);
+    objects=results;
+}
+
+function back(){
+    window.location="index.html";
+}
